4ce4fa74a1ffe9dc8d8fe56fc51e30beed0a8342
joaco <j.gonzalez.bossi@itsv.edu.ar>
2026-08-24 15:07:51 +0000

    modifico el inicio para la clase del 24/08

--- tiendalibre/views.py
+++ tiendalibre/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
 from django.views.generic import TemplateView
-
+from .models import Producto
 
 class TiendaTemplateView(TemplateView):
     template_name = 'tienda.html'
 
+def home_1(request):
+    return HttpResponse("<h1>Bienvenido a la tienda Libre de Maximo Corallo Margosian</h1>")
 
 def home(request):
-    return render(request, 'tiendalibre/home.html')
-
+    contexto = {
+        'productos': Producto.objects.all()[:6],
+        'titulo': 'Tienda Libre',
+        'esta_logueado': True, #False muestra un mensaje que se necesita estar logueado
+    }
+    return render(request, 'tiendalibre/home.html', contexto)
 
 def acerca_de_mi(request):
     return render(request, 'tiendalibre/acerca-de-mi.html')
